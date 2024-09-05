7bf7a4e39aff4faf6bf60d926e3452e30d931d01
tushar-03 <tushar65635@gmail.com>
2024-09-05 15:00:55 +0530

    cors changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000', // Adjust this to your frontend URL if different
+    origin: 'https://video-player-frontend-g5we.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
