16e716bf778ff23593eb59b00b2400cff755c979
tushar-03 <tushar65635@gmail.com>
2024-09-05 15:05:44 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://video-player-frontend-g5we.vercel.app/',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
